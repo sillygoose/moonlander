b4401b453967f8ce4acafc9be03cf0d115ef00aa
Rick <rick@fivemilepoint.com>
2011-06-14 13:16:41 -0400

    Class used to manage the dust kickup

---
+++ Moonlander/Dust.h
@@ -0,0 +1,17 @@
+//
+//  Dust.h
+//  Moonlander
+//
+//  Created by Silly Goose on 6/14/11.
+//  Copyright 2011 Silly Goose Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "VGView.h"
+
+@interface Dust : VGView {
+    
+}
+
+@end
---
+++ Moonlander/Dust.m
@@ -0,0 +1,14 @@
+//
+//  Dust.m
+//  Moonlander
+//
+//  Created by Silly Goose on 6/14/11.
+//  Copyright 2011 Silly Goose Software. All rights reserved.
+//
+
+#import "Dust.h"
+
+
+@implementation Dust
+
+@end
