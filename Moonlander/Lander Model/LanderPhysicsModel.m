952b2b582573b2e12a09c6f66f6d440fd9ec9d8c
Rick Naro <rick@fivemilepoint.com>
2012-03-28 11:35:38 -0400

    Added macro control over debug start point using DEBUG_FLAME, DEBUG_DUST, and DEBUG_LOCATION

--- Moonlander/Lander Model/LanderPhysicsModel.m
+++ Moonlander/Lander Model/LanderPhysicsModel.m
@@ -237,18 +237,20 @@ float RadiansToDegrees(float radians)
 #pragma mark Model initialization
 - (void)initializeLanderModel
 {
-#if 0
+#if defined(DEBUG_DUST) || defined(DEBUG_FLAME) || defined(DEBUG_LOCATION)
+    // Custom lander start point
     self.rateOfTurn = 0;
-    self.turnAngle = DegreesToRadians(0.0f);
+    self.turnAngle = DegreesToRadians(46.0f);
     self.horizontalVelocity = 0;
-    self.verticalVelocity = -8;
+    self.verticalVelocity = 0;
     self.horizontalDistance = 70;
-    self.verticalDistance = 75;
-    self.percentThrustRequested = 15;
+    self.verticalDistance = 120;
+    self.percentThrustRequested = 18;
     self.actualThrust = self.percentThrustRequested * self.maxThrust / 100.0;
     self.fuelRemaining = self.lemInitalFuel;
     self.clockTicks = 0.0f;
 #else
+    // Default game start point
     self.rateOfTurn = 0.0f;
     self.turnAngle = DegreesToRadians(-70.0f);
     self.horizontalVelocity = 1000.0f;
