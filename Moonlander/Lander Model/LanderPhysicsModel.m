5e767bc484cd31303546e09801ef039ce5968efb
Rick Naro <rick@fivemilepoint.com>
2012-04-11 12:13:55 -0400

    Fixed thruster display

--- Moonlander/Lander Model/LanderPhysicsModel.m
+++ Moonlander/Lander Model/LanderPhysicsModel.m
@@ -262,13 +262,14 @@ float RadiansToDegrees(float radians)
 #if defined(DEBUG_DUST) || defined(DEBUG_FLAME) || defined(DEBUG_LOCATION)
     // Custom lander start point
     self.rateOfTurn = 0;
-    self.turnAngle = DegreesToRadians(46.0f);
+    self.turnAngle = DegreesToRadians(0);
     self.horizontalVelocity = 0;
     self.verticalVelocity = 0;
     self.verticalVelocity = 0;
-    self.horizontalDistance = 70;
+    self.horizontalDistance = 220;
     self.verticalDistance = 120;
     self.percentThrustRequested = 18;
+    self.actualThrust = self.percentThrustRequested * self.maxThrust / 100.0;
     self.fuelRemaining = self.lemInitalFuel;
     self.clockTicks = 0.0f;
 #else
