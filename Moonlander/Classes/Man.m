09554eaf5067184284182b3a6dbb0e72fee87b57
Rick Naro <rick@fivemilepoint.com>
2012-04-26 13:14:20 -0400

    Fixed up code that used the frame when the bounds should have been used

--- Moonlander/Classes/Man.m
+++ Moonlander/Classes/Man.m
@@ -3,7 +3,7 @@
 //  Moonlander
 //
 //  Created by Rick Naro on 9/7/11.
-///  Copyright 2011, 2012 Paradigm Systems. All rights reserved.
+//  Copyright 2011, 2012 Paradigm Systems. All rights reserved.
 //
 
 #import "Man.h"
@@ -23,8 +23,8 @@
         self.drawPaths = [manDict objectForKey:@"paths"];
         self.vectorName = @"[Man init]";
         
-        // Adjust the center for the width of the man
-        self.center = CGPointMake(self.center.x - (self.frame.size.width / 2), self.center.y);//###bounds?
+        // Adjust the center for the width of the man view
+        self.center = CGPointMake(self.center.x - (self.bounds.size.width / 2), self.center.y);
     }
     return self;
 }
